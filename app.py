bae62e936f78c370e8d578e822e9ba97f1e3bf3c
Tehrim-16 <tehrimshaikh76@gmail.com>
2025-08-21 21:47:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
